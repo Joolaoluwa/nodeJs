27a92e75eff53cb7957ce7a07c001203b5143ba9
olusola joshua <joolaoluwa2002@gmail.com>
2024-08-29 15:54:29 +0100

    First lesson

---
+++ lesson1/nodejs1.js
@@ -0,0 +1,20 @@
+console.log("Wagwan");
+
+let timer = 0
+/** 
+ * About the Global Object, I used setTimeOut, 
+ * setInterval, and clearInterval to do a particular thing for a
+ * given amount of time and to write code that works according to a particular timing
+ * 
+ * Prohramming itself is a tool, forget code sometimes and give more time to thought
+ * */ 
+let timing = setInterval(() => {
+    timer += 2
+    console.log(timer + " seconds remaining")
+    if (timer > 6)
+    {
+        clearInterval(timing)
+    }
+}, 2000);
+
+console.log(__dirname);
