f7db3529e874795aab6482521201a57e45bd617e
olusola joshua <joolaoluwa2002@gmail.com>
2024-09-04 13:23:52 +0100

    new changes

---
+++ client-servers/clienServer.js
@@ -0,0 +1,8 @@
+/**
+ * In order to serve files over a particular network, there exists the 
+ * client-server model, this serves as a source of communication between two computers
+ * where the client sends a request and the server recieves the request and sends a response to the client.
+ * They do this using set communication rules that both sides understand and are called protocols.
+ * For nodejs to know what response to give to propert request, it must be listening on a particular port
+ * 
+ */
